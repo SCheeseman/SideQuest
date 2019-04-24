367672bfd720473ffe5a67340696c26bf9729bb3
Shane Harris <shane@xactaccounts.co.uk>
2019-04-24 21:00:40 +0100

    added json db files, will run from github for a while.

--- desktop-app/js/app.js
+++ desktop-app/js/app.js
@@ -1,1 +1,15 @@
-M.AutoInit();
+
+class App{
+    constructor(){
+        M.AutoInit();
+        this.getItems("","","all")
+    }
+    getItems(type,search,category){
+        // fetch()
+        //     .then(res=>res.json())
+        //     .then(json=>{
+        //         console.log(json);
+        //     })
+    }
+}
+new App();
